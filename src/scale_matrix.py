6f04e7f8dceed0cceda01845a2e1ebda9715847f
JUHO <ijuho9555@gmail.com>
2025-07-31 10:23:46 +0900

    행렬을 이용한 이미지 확대 축소
    0.5배 축소 행렬과 2배 확대 행렬을 이용한 이미지 확대 축소

---
+++ src/scale_matrix.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('../img/fish.jpg')
+height, width = img.shape[:2]  # 영상의 크기
+
+# 0.5배 축소 변환 행렬
+m_small = np.float32([[0.5, 0, 0],
+                      [0, 0.5, 0]])
+
+# 2배 확대 변환 행렬
+m_big = np.float32([[2, 0, 0],
+                    [0, 2, 0]])
+
+# 보간법 적용없이 확대, 축소 (보간법: 이동할 거리 예측)
+dst1 = cv2.warpAffine(img, m_small, (int(height*0.5), int(width*0.5)))
+dst2 = cv2.warpAffine(img, m_big, (int(height*2), int(width*2)))
+
+# 보간법 적용한 확대, 축소
+dst3 = cv2.warpAffine(img, m_small, (int(height*0.5), int(width*0.5)), None, cv2.INTER_AREA)
+dst4 = cv2.warpAffine(img, m_big, (int(height*2), int(width*2)), None, cv2.INTER_CUBIC)
+
+cv2.imshow('original', img)
+cv2.imshow('small', dst1)
+cv2.imshow('big', dst2)
+cv2.imshow('small INTER_AREA', dst3)
+cv2.imshow('big INTER_CUBIC', dst4)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
