df531fc5e1ea223df9db004bba253c1bf045d566
JUHO <ijuho9555@gmail.com>
2025-07-31 16:09:26 +0900

    프리윗 필터

---
+++ src/15.edge_prewitt.py
@@ -0,0 +1,20 @@
+# 프리윗 마스크로 경계 검출 
+
+import cv2
+import numpy as np
+
+img = cv2.imread("../img/sudoku.jpg")
+
+# 프리윗 커널 생성
+gx_kernel = np.array([[ -1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+gy_kernel = np.array([[-1, -1, -1],[0, 0, 0], [1, 1, 1]])
+
+# 필터 적용
+edge_gx = cv2.filter2D(img, -1, gx_kernel)
+edge_gy = cv2.filter2D(img, -1, gy_kernel)
+
+# 결과 출력
+merged = np.hstack((img, edge_gx, edge_gy, edge_gx+edge_gy))
+cv2.imshow('prewitt', merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
