6ad08711e4fbf450c0b66289b3bf52bf3fe17ebc
JUHO <ijuho9555@gmail.com>
2025-07-31 16:31:37 +0900

    이미지를 불러와 그레이스케일로 설정

--- src/21.plate_processor.py
+++ src/21.plate_processor.py
@@ -4,3 +4,12 @@ import os
 
 input_dir = '../extracted_plates'
 output_dir = '../processed_plates'
+
+def process_plate_image(img_path):
+    img = cv2.imread(img_path)
+    if img is None:
+        print(f'이미지 불러오기 실패: {img_path}')
+        return
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
