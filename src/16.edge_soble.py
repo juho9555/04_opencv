3935a53dfdc167dd974fe869e1f1ff0e28cbf099
JUHO <ijuho9555@gmail.com>
2025-07-31 16:09:33 +0900

    소벨 필터

---
+++ src/16.edge_soble.py
@@ -0,0 +1,26 @@
+# 소벨 마스크로 경계 검출 
+
+import cv2
+import numpy as np
+
+img = cv2.imread("../img/sudoku.jpg")
+
+# 소벨 커널 생성
+gx_kernel = np.array([[ -1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+gy_kernel = np.array([[-1, -2, -1],[0, 0, 0], [1, 2, 1]])
+
+# 필터 적용
+edge_gx = cv2.filter2D(img, -1, gx_kernel)
+edge_gy = cv2.filter2D(img, -1, gy_kernel)
+
+# 소벨 API를 생성해서 엣지 검출
+sobelx = cv2.Sobel(img, -1, 1, 0, ksize=3)
+sobely = cv2.Sobel(img, -1, 0, 1, ksize=3)
+
+# 결과 출력
+merged1 = np.hstack((img, edge_gx, edge_gy, edge_gx+edge_gy))
+merged2 = np.hstack((img, sobelx, sobely, sobelx+sobely))
+merged = np.vstack((merged1, merged2))
+cv2.imshow('Sobel', merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
